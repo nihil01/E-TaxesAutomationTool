6ae76f070cb21b62f833ddea4703a53df316d295
Orkhan <110621098+nihil01@users.noreply.github.com>
2025-09-25 13:18:51 +0400

    Create LoggingService.java

---
+++ src/main/java/com/bizcon/taxesautomator/services/LoggingService.java
@@ -0,0 +1,18 @@
+package com.bizcon.taxesautomator.services;
+
+import com.bizcon.taxesautomator.utils.MessageType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingService {
+
+    private static Logger logger = LogManager.getLogger(LoggingService.class);
+
+    public static void logData(String message, MessageType logType) {
+        switch (logType){
+            case WARN -> logger.warn(message);
+            case ERROR -> logger.error(message);
+            case INFO -> logger.info(message);
+        }
+    }
+}
