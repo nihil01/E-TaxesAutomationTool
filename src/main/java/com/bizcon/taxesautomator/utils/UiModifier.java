af9823156a8932e8510fd22fd8175748e4d2d210
Orkhan <110621098+nihil01@users.noreply.github.com>
2025-09-26 17:00:15 +0400

    Update UiModifier.java

--- src/main/java/com/bizcon/taxesautomator/utils/UiModifier.java
+++ src/main/java/com/bizcon/taxesautomator/utils/UiModifier.java
@@ -1,9 +1,11 @@
 package com.bizcon.taxesautomator.utils;
 
+import java.util.HashSet;
+
 public interface UiModifier {
 
     void markAsCompleted(String rowNumber);
     void markAsFailed(String rowNumber);
-    void notifyCompletion(String message);
+    void notifyCompletion(String message, HashSet<String> asanIDs);
 
 }
